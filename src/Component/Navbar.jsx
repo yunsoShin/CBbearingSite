03e692618a7b01af57038a3da2290dfd80f04342
yunsoo1 <shin4895@gmail.com>
2023-02-13 11:18:00 +0900

    title
    타이틀 로고와 타이틀이름 설정,Navbar의 user정보 반응형으로 나타내기

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState } from 'react';
 import { Outhlogin,Outhlogout,onUserStateChange } from '../Api/OuthAPI';
 import CompanyLogo from '../img/CompanyNavLogo.png'
 import { Link } from 'react-router-dom';
+import User from './User';
 export default function Navbar() {
     const[user,setUser] = useState();
     useEffect(()=>{
@@ -9,15 +10,13 @@ export default function Navbar() {
     },[]);
 
     return (
-
         <header className="flex justify-between p-2">
-            
             <nav className='flex justify-between p-0 w-full items-center gap-3 border-b border-gray-300'>
                 <Link to='/'><img src={CompanyLogo} alt="CompanyLogo"/></Link>
                 <ul className='flex items-center gap-5 font-semibold text-xl pr-5'>
-                    
-                    <li>{!user && <button onClick={Outhlogin}>login</button>}</li>
-                    <li>{user && <button  onClick={Outhlogout}>logout</button>}</li>
+                    <li>{user&&<User user={user}/>}</li>
+                    <li>{!user && <button onClick={Outhlogin}>Login</button>}</li>
+                    <li>{user && <button  onClick={Outhlogout}>Logout</button>}</li>
                     <li><Link to='/Brand'>Brand</Link></li>
                     <li><Link to='/Contact'>Contact</Link></li>
                     <li><Link to='/products'>product</Link></li>
