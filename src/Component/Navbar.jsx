d52e73eec6d5e61c7d830234ba72ebe243dcf059
yunsoo1 <shin4895@gmail.com>
2023-03-23 00:09:49 +0900

    navbar CSS

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -11,9 +11,9 @@ export default function Navbar() {
     
 
     return (
-        <header className='flex justify-between border-b border-gray-300 p-0'>
+        <header className='flex justify-between border-b border-gray-300 w-full'>
             <Link to='/'><img className='flex items-center scale-75 translate-x-[-1rem]'  src={CompanyLogo} alt="CompanyLogo" /></Link>
-            <nav className='flex items-center gap-4 font-semibold text-xl '>
+            <nav className='flex items-center gap-8 font-semibold text-xl px-10'>
                     {user&&<User user={user}/>}
                     {!user && <button onClick={Outhlogin}>Login</button>}
                     {user && <button  onClick={Outhlogout}>Logout</button>}
