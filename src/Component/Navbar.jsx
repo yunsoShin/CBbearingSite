bcb947352eee9e1f5bafb2069df6d473ecd9a2c9
yunsoo1 <shin4895@gmail.com>
2023-03-23 12:07:49 +0900

    navbar CSS

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,34 +2,27 @@ import React, { useEffect, useState } from 'react';
 import { login,logout, onUserStateChange } from '../Api/firebase';
 import CompanyLogo from '../img/CompanyNavLogo.png'
 import { Link } from 'react-router-dom';
+import User from './User';
 
 
 export default function Navbar() {
     const[user,setUser] = useState();
     useEffect(()=>{
-        onUserStateChange((user)=>{
-            console.log(user);
-            setUser(user);
-        });
+        onUserStateChange(setUser);
     }, [])
-    const handlelogin = ()=>{
-        login().then(user=>setUser);
-    }
-    const handlelogout = ()=>{
-        logout().then(user=>setUser);
-    }
-    
 
     return (
-        <header className='flex justify-between border-b border-gray-300 w-full translate-y-[-0.5rem]'>
-            <Link to='/'><img className='flex items-center scale-75 translate-x-[-1rem]'  src={CompanyLogo} alt="CompanyLogo" /></Link>
+        <header className='flex justify-between border-b border-gray-300 w-full h-20'>
+            <Link to='/'><img className='items-center  hidden sm:block h-12 scale-120 translate-x-3 translate-y-3'  src={CompanyLogo} alt="CompanyLogo" /></Link>
             <nav className='flex items-center gap-8 font-semibold text-xl px-10'>
                     
-                    {!user && <button onClick={handlelogin}>Login</button>}
-                    {user && <button  onClick={handlelogout}>Logout!</button>}
                     <Link to='/Brand'>Brand</Link>
                     <Link to='/Contact'>Contact</Link>
                     <Link to='/products'>product</Link>
+                    {user && <User user={user}></User>}
+                    {!user && <button onClick={login}>Login</button>}
+                    {user && <button  onClick={logout}>Logout!</button>}
+                    
             </nav>
         </header>
     );
--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default function User({user:{photoURL,displayName}}) {
     return (
         <div className='flex items-center'>
-            <img className='w-10 h-10 rounded-full mr-2 hidden md:block' src={photoURL} alt={displayName} />
-            <span className='hidden md:block'>{displayName}</span>
+            <img className='w-10 h-10 rounded-full hidden md:block' src={photoURL} alt={displayName} />
         </div>
     );
 }
