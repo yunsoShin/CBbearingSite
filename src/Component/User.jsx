f7a2fa95f4904bdbdf5e5ad987488f03d24274a4
yunsoo1 <shin4895@gmail.com>
2023-04-02 23:47:52 +0900

    Update User.jsx

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 
 export default function User({user:{photoURL,displayName}}) {
     return (
-        
+        <div className='flex items-center'>
+            <img className='w-10 h-10 rounded-full hidden md:block' src={photoURL} alt={displayName} />
+        </div>
+    );
+}
 
