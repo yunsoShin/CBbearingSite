98ab74f267c33fb864bb5e15320ad75f5d29741b
yunsoo1 <shin4895@gmail.com>
2023-04-04 23:49:42 +0900

    Update Button.jsx

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Button({text,onClick}) {
     return (
-        <button onClick={onClick}>
+        <button className='' onClick={onClick}>
             {text}
-        </button>
+         </button>
     );
 }
 
