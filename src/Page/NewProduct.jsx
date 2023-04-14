d425f39a0de231fcd97eb3550046950e75a67d05
shin yun soo <shin4895@gmail.com>
2023-04-14 20:10:21 +0900

    NewProduct input

--- src/Page/NewProduct.jsx
+++ src/Page/NewProduct.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function NewProduct() {
-  return <div>New Products</div>;
+  const [product,setProduct]=useState({});
+  const [file,setFile]=useState();
+  const handleChange=(e)=>{
+    const {name,value,files}=e.target;
+    if(name==='file'){
+      setFile(files&&files[0]);
+      return;
+    }
+    setProduct((product)=>({...product,[name]:value}))
+  };
+  const handleSubmit=(e)=>{};
+  return <section>
+    <form onSubmit={handleSubmit}>
+      <input 
+        type='file'
+        accept='image/*'
+        name='file'
+        required
+        onChange={handleChange}
+        />
+        <input type="text" name='rirle' value={product.title ?? ''} placeholder='제품명'required onChange={handleChange}/>
+    </form>
+  </section>;
 }
