8b1578c0e6c1ab89556724bf89ada4c2da53412f
Shin <dbstn5053@gmail.com>
2023-04-14 23:30:59 +0900

    오타 정정

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Button({text1,onClick}) {
+export default function Button({text,onClick}) {
     return (
         <button className='border border-black w-1/2 hover:bg-black hover:text-white p-1 px-3' onClick={onClick}>
             {text}
