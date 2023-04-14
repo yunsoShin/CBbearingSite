735dd1e9ba6fb78d687a46d9bd7309f4ba4f9b91
Shin <dbstn5053@gmail.com>
2023-04-14 23:05:10 +0900

    Update Button.jsx

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Button({text,onClick}) {
+export default function Button({text1,onClick}) {
     return (
         <button className='border border-black w-1/2 hover:bg-black hover:text-white p-1 px-3' onClick={onClick}>
             {text}
-         </button>
+        </button>//커밋할 내용
     );
 }
 
