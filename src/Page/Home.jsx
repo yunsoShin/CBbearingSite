957ab59e14e356f256daf5d8abbccf7c3e4462a5
Shin <dbstn5053@gmail.com>
2023-04-17 14:48:35 +0900

    logoBar swiper 기능 구현

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Products from '../Component/Products';
 import Banner from '../Component/Banner';
+import LogoSlider from '../Component/LogoSlider';
 
 export default function Home() {
   return <>
     <Banner></Banner>
     <Products></Products>
+    <LogoSlider></LogoSlider>
   </>
 }
