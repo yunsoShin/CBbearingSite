bc66444dd6593ca6372230847988987205d9485a
Shin <dbstn5053@gmail.com>
2023-04-18 18:07:57 +0900

    삭제버튼 확인하는 팝업생성

--- src/Component/ProductCard.jsx
+++ src/Component/ProductCard.jsx
@@ -13,7 +13,10 @@ function ProductCard({
         <h3 className='truncate'>{title}</h3>
         <p className='mt-2 px-2'>{brand}</p>
         <p>{country}</p>
-        {user && user.isAdmin && <button onClick={()=> handleDelete(product.id)}>삭제하기</button>}
+        {user && user.isAdmin && <button onClick={()=> 
+          window.confirm("삭제하시겠습니까?") ? handleDelete(product.id)
+          .then(window.alert("삭제완료하였습니다"))
+          .catch(window.alert("다시시도해 주세요")) : false }>삭제하기</button>}
       </div>
       
     </li>
