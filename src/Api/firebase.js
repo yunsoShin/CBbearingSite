ea9d5bc252b95c4ba6b7f81dbbf1ca7de82fb6d6
yunsoo1 <shin4895@gmail.com>
2023-04-22 22:51:12 +0900

    Update firebase.js

--- src/Api/firebase.js
+++ src/Api/firebase.js
@@ -1,31 +1,26 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider , signOut , onAuthStateChanged} from "firebase/auth";
-import { getDatabase, ref, set, get } from "firebase/database";
-import {v4 as uuid} from "uuid";
+import { getDatabase, ref, get } from "firebase/database";
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
 };
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const database = getDatabase(app);
 
-
-
-export function loginGoogle(){
+export function login(){
   signInWithPopup(auth, provider).catch(console.error);
-}  
+} 
 
 
-export function logoutGoogle(){
+export function logout(){
   signOut(auth)
   
 } 
@@ -38,7 +33,7 @@ export function onUserStateChange(callback){
   });
 }
 
-function adminUser(user){
+async function adminUser(user){
   return get(ref(database,'admins'))
   .then((snapshot)=>{
     if(snapshot.exists()) {
@@ -47,53 +42,29 @@ function adminUser(user){
       return{...user,isAdmin};
     }
     return user;
-  })
-}
-
-export async function addNewProduct(product,image=''){
-  const id=uuid();
-  return set(ref(database,`products/${id}`),{
-    ...product,
-    id,
-    image,
-    option: product.options ? product.options.split(',') : [],
-  });
-}
-
-export async function getProducts(){
-  return get(ref(database,'products')).then((snapshot)=>{
-        if(snapshot.exists()) {
-          return Object.values(snapshot.val());
-        }
-        return [];
-      })
-}
-export async function handleDelete(id) {
-  const itemRef = ref(database, `products/${id}`);
-  return set(itemRef, null)
-    .then(() => {
-      console.log('Data deleted successfully');
-    })
-    .catch((error) => {
-      console.error('Data deletion failed:', error);
-    });
+    //dsdsd
+  })  
 }
 
 
 
-export async function addNewQuestion(question){
-  const id=uuid();
-  return set(ref(database,`questions/${id}`),{
-    ...question,
-    id,
-  });
+
+
+
+
+/*export function Outhlogin() {
+  signInWithPopup(auth, provider).catch(console.error);
 }
 
-export function getQuestion(){
-  return get(ref(database,'questions')).then((snapshot)=>{
-        if(snapshot.exists()) {
-          return Object.values(snapshot.val());
-        }
-        return [];
-      })
-    }
+
+export function Outhlogout() {
+  signOut(auth).catch(console.error);
+}
+
+
+export function onUserStateChange(callback) {
+    callback();
+}
+
+*/
+
