a91d442b07dd8c7742e440be2f477d6254a1a825
Shin <shin4895@gmail.com>
2023-04-28 23:44:58 +0900

    Swiper component update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Banner from '../components/Banner';
 import Products from '../components/Products';
 import OnlineQuestion from '../components/OnlineQuestion';
-
+import SwiperLogo from '../components/SwiperLogo';
 export default function Home() {
   return (
     <>
       <Banner />
       <Products />
       <OnlineQuestion></OnlineQuestion>
+      <SwiperLogo></SwiperLogo>
     </>
   );
 }
