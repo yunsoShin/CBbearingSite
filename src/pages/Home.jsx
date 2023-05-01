85ea2ab4d344c9ae7c700c912c87cb2385d59e8c
Shin <shin4895@gmail.com>
2023-05-01 22:53:00 +0900

    필요없는 컴포넌트 삭제

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import Products from '../components/Products';
 import OnlineQuestion from '../components/OnlineQuestion';
 import SwiperLogo from '../components/SwiperLogo';
 import FooterBottom from '../components/FooterBottom';
-import ProductSection from '../components/ProductSection';
+
 export default function Home() {
   return (
     <>
       <Banner />
-      <ProductSection></ProductSection>
+      
       <OnlineQuestion></OnlineQuestion>
       <SwiperLogo></SwiperLogo>
       <FooterBottom></FooterBottom>
