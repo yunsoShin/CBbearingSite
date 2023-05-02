ca68d30e2dec0ace4f357cadb1705ec3512535b6
Shin <shin4895@gmail.com>
2023-05-02 23:24:15 +0900

    ant design을 사용해. tablepage구축

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ProductCard from './ProductCard';
 import useProducts from '../hooks/useProducts';
+import Button from './ui/Button';
 
 export default function ProductList() {
   const ITEMS_PER_PAGE = 30;
@@ -10,32 +11,52 @@ export default function ProductList() {
   } = useProducts();
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
+  const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
+
   const goToNextPage = () => {
     setCurrentPage((prevPage) => prevPage + 1);
   };
 
   const goToPreviousPage = () => {
     setCurrentPage((prevPage) => prevPage - 1);
-  }; //테스트1
-  
+  };
+
+  const goToPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <Button
+          key={i}
+          onClick={() => goToPage(i)}
+          text={i}
+          disabled={i === currentPage}
+        />,
+      );
+    }
+    return pageNumbers;
+  };
+
   return (
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       <ul className='grid grid-cols-1 md:grid-cols-1  gap-4 p-4 w-full'>
         {products &&
-     products.slice(startIndex,endIndex).map((product) => (
-      <ProductCard key={product.id} product={product} />
-    ))}
+          products.slice(startIndex, endIndex).map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
       </ul>
-      <button onClick={goToPreviousPage} disabled={currentPage === 1}>
-        이전 페이지
-      </button>
-      <button
+      <Button onClick={goToPreviousPage} disabled={currentPage === 1} text={'이전 페이지'} />
+      {renderPageNumbers()}
+      <Button
         onClick={goToNextPage}
-      >
-        다음 페이지
-      </button>
+        disabled={currentPage === totalPages}
+        text={'다음 페이지'}
+      />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import NotFound from './pages/NotFound';
 import ProtectedRoute from './pages/ProtectedRoute';
 import AllQuestions from './pages/AllQuestions';
 import NewQuestion from './pages/NewQuestion';
+import 'antd/dist/antd.css';
 
 const router = createBrowserRouter([
   {
