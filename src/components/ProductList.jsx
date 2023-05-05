53ef6e1df1adc41442abf78e6e14cd1f3aa2918f
Shin <shin4895@gmail.com>
2023-05-05 23:49:00 +0900

    name 리팩토링

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,43 +1,134 @@
-import React, { useState } from 'react';
-import ProductCard from './ProductCard';
+import React, { useRef, useState } from 'react';
+import '../index.css';
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Table } from 'antd';
+import Highlighter from 'react-highlight-words';
 import useProducts from '../hooks/useProducts';
-import Button from './ui/Button';
-export default function ProductList() {
-  const ITEMS_PER_PAGE = 30;
-  const [currentPage, setCurrentPage] = useState(1);
+
+
+
+export default function ProductList(){
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
   const {
     productsQuery: { isLoading, error, data: products },
   } = useProducts();
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const endIndex = startIndex + ITEMS_PER_PAGE;
-  const goToNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-    console.log(currentPage);
+  const data = products
+  console.log(data)
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
   };
 
-  const goToPreviousPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
-    console.log(currentPage);
-  }; //테스트1
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex] &&
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 10 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+        ) : (
+          text
+        ),
+    });
+    const columns = [
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        key: 'title',
+        width: '30%',
+        ...getColumnSearchProps('title'),
+      },
+      {
+        title: 'Brand',
+        dataIndex: 'brand',
+        key: 'brand',
+        width: '20%',
+        ...getColumnSearchProps('brand'),
+      },
+      {
+        title: 'partNumber',
+        dataIndex: 'partNumber',
+        key: 'partNumber',
+        ...getColumnSearchProps('partNumber'),
+        
+        
+      },
+    ];
   
+    return <Table  className='' columns={columns} dataSource={data}  />;
+  };
   
-  return (
-    <>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      <ul className='grid grid-cols-1 md:grid-cols-1  gap-4 p-4 w-full'>
-        {products&&products.slice(startIndex,endIndex).map((product) => (
-      <ProductCard key={product.id} product={product} />
-    ))}
-      </ul>
-      <Button onClick={goToPreviousPage} disabled={currentPage === 1} text={'이전 페이지'}>
-      </Button>
-      
-      <Button
-        onClick={goToNextPage}
-        text={'다음 페이지'}
-      >
-      </Button>
-    </>
-  );
-}
--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ProductList from '../components/ProductList';
-import AntDesign from '../components/AntDesigntest';
+
 
 export default function AllProducts() {
-  return <AntDesign></AntDesign>;
+  return <ProductList></ProductList>;
+  
 }
