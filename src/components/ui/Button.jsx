cdbcd65c990bb927acca3411cbba7459f16cf6cd
Shin <shin4895@gmail.com>
2023-05-05 23:46:37 +0900

    빌드하기

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ text, onClick }) {
   return (
     <button
       className=' text-white py-2 px-4 rounded-md hover:brightness-150  bg-slate-800'
-      onClick={onClick}
+      onClick={onClick} 
     >
       {text}
     </button>
