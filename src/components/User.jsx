73681691ddd14de9c02ccfcf94d6bd93eaa0bde8
Shin <shin4895@gmail.com>
2023-05-06 22:23:43 +0900

    Updates

--- src/components/User.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function User({ user: { displayName } }) {
-  return (
-    <div className='flex items-center shrink-0'>
-      <span>{displayName}</span>
-    </div>
-  );
-}
--- src/pages/AllProducts.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ProductList from '../components/ProductList';
-
-
-export default function AllProducts() {
-  return <ProductList></ProductList>;
-  
-}
